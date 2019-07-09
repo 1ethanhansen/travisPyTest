fa3ebb2c4ccf42cd8227c1d6feee791b4326be9e
EthanHans3n <1ethanhansen@gmail.com>
2019-07-09 10:03:06 -0700

    consistent?

--- factorial.py
+++ factorial.py
@@ -4,10 +4,10 @@
 
 def factorial(number):
 	if (number > 0) :
-    		while (counter > 1) :
-        		total *= counter
-        		counter -= 1
-    		return total
+		while (counter > 1) :
+			total *= counter
+			counter -= 1
+		return total
 	else:
-    		print("that is an invalid input")
+		print("that is an invalid input")
 		return 0
