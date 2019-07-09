a3f96adc35166ac3e5fcb5b9bb4d2f0aaceb7d90
EthanHans3n <1ethanhansen@gmail.com>
2019-07-09 10:09:20 -0700

    actually testing

--- testFactorial.py
+++ testFactorial.py
@@ -2,11 +2,8 @@ import pytest
 
 from factorial import factorial
 
-def test_factorial_6():
-	assert(factorial(6) == 720)
+assert(factorial(6) == 720)
 
-def test_factorial_8():
-	assert(factorial(8) == 40320)
+assert(factorial(8) == 40320)
 
-def test_factorial_10():
-	assert(factorial(10) == 3628800)
+assert(factorial(10) == 3628800)
