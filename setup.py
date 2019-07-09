7058763f5a0e14f11cf2fa590c6f431dd54ecb44
EthanHans3n <1ethanhansen@gmail.com>
2019-07-09 14:06:07 -0700

    find_packages

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import setup
+from setuptools import find_packages
 
 setup(name='factorial',
       version='0.1',
       description='Simple factorial program',
       author='Ethan Hansen',
-      packages=['factorial'],
+      packages=find_packages(),
       zip_safe=False)
